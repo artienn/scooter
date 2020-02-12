54db02b72b65fd96190c8002760da262cf36c688
MusAushev <mustafa-aushev@mail.ru>
2020-02-12 18:13:53 +0300

    fix

--- libs/facebook.js
+++ libs/facebook.js
@@ -3,18 +3,20 @@ const {facebook} = require('../config');
 
 exports.checkCode = async (code) => {
     const options = {
-        uri: `https://graph.facebook.com/v6.0/oauth/access_token?client_id=${facebook.appID}&redirect_uri=${facebook.callbackUrl}&client_secret=${facebook.appSecret}&code=${code}`
+        uri: `https://graph.facebook.com/v6.0/oauth/access_token?client_id=${facebook.appID}&redirect_uri=${facebook.callbackUrl}&client_secret=${facebook.appSecret}&code=${code}`,
+        json: true
     };
     return rpn(options)
         .then(result => {
-            console.log(result);
+            return result;
         });
 };
 
 exports.data = async (token) => {
     const options = {
         uri: `https://graph.facebook.com/debug_token?input_token=${token}
-        &access_token=120633292693716|11fHBVihNknxxTHou26gVaZEoUw`
+        &access_token=120633292693716|11fHBVihNknxxTHou26gVaZEoUw`,
+        json: true
     };
     return rpn(options) 
         .then(result => {
