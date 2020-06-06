b476cee00f40b9f0ca2809b4f5761b9230f31531
vvdev <vvdvev@vvdevs-MacBook-Pro.local>
2020-06-06 13:47:23 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.use('/scooters', require('./scooters'));
 router.use('/contracts', require('./contracts'));
 router.use('/files', require('./files'));
 router.use('/zones', require('./zones'));
+router.use('/admins', require('./admins'));
 
 module.exports = router;
