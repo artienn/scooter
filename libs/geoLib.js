b44871cb313377bbfd38c9f21d68a3a71b028051
vvdev <vvdvev@vvdevs-MacBook-Pro.local>
2020-06-12 14:56:29 +0300

    fix

--- libs/geoLib.js
+++ libs/geoLib.js
@@ -3,9 +3,9 @@ const Distance = require('geo-distance');
 const DISTANCE_BETWEEN_USER_AND_SCOOTER = 5;
 exports.DISTANCE_BETWEEN_USER_AND_SCOOTER = DISTANCE_BETWEEN_USER_AND_SCOOTER;
 
-exports.pointInsideZones = async (point, zone) => {
-    if (point && zone) {
-        const result = await geoLib.pointInsidePolygon(point, zone.coordinates);
+exports.pointInsideZones = async (point, coordinates) => {
+    if (point && coordinates) {
+        const result = await geoLib.pointInsidePolygon(point, coordinates);
         if (result) return true;
     }
     return false;
