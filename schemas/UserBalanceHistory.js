6d118afb738adffbf1f3458141fd3197536a6c16
Mus <mustafa-aushev@mail.ru>
2020-07-10 09:03:27 +0300

    fix

--- schemas/UserBalanceHistory.js
+++ schemas/UserBalanceHistory.js
@@ -11,7 +11,8 @@ const UserBalanceHistory = new Schema({
         type: String,
         enum: [
             'pay',
-            'contract'
+            'contract',
+            'admin'
         ]
     },
     contract: {
